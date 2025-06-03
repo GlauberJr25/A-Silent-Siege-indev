8237b0e08df7d8a582d8026a3c250f2e15b8aece
GlauberJr25 <Glauberjr12@gmail.com>
2025-06-03 13:11:06 -0300

    Domain Engineering Hullmod added

--- src/poggers/oldlegion/data/hullmods/DomainEngineering.java
+++ src/poggers/oldlegion/data/hullmods/DomainEngineering.java
@@ -1,17 +1,9 @@
 package poggers.oldlegion.data.hullmods;
 
-import java.awt.Color;
-
 import com.fs.starfarer.api.combat.BaseHullMod;
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
-import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.impl.campaign.ids.Stats;
-import com.fs.starfarer.api.ui.Alignment;
-import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.api.util.Misc;
 
 public class DomainEngineering extends BaseHullMod {
 
