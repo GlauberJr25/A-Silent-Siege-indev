7ea679bd1ed42ecbe5a29ad6c7a3fb4743ef173f
NebulaModLab <NebulaModding@hotmail.com>
2025-08-14 18:02:55 +0200

    Custom Entity Comm Relay & Radiation

--- src/poggers/oldlegion/world/systems/MySystemOne.java
+++ src/poggers/oldlegion/world/systems/MySystemOne.java
@@ -18,6 +18,8 @@ import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.impl.campaign.terrain.DebrisFieldTerrainPlugin;
 import com.fs.starfarer.api.impl.campaign.terrain.DebrisFieldTerrainPlugin.DebrisFieldParams;
 
+import com.fs.starfarer.api.impl.campaign.terrain.EventHorizonPlugin;
+import com.fs.starfarer.api.impl.campaign.terrain.MagneticFieldTerrainPlugin;
 import com.fs.starfarer.api.util.Misc;
 import org.apache.log4j.Logger;
 
@@ -30,25 +32,96 @@ public class MySystemOne {
     public static Logger log = Global.getLogger(MySystemOne.class);
 
     public void generate(SectorAPI sector) {
-        StarSystemAPI domainOutpost = sector.createStarSystem("Nataruk");
-        domainOutpost.getLocation().set(0,-65000); //bottom centerish - was +80000,-55000
+        StarSystemAPI domainOutpostSystem = sector.createStarSystem("Nataruk");
+        domainOutpostSystem.getLocation().set(0,-65000); //bottom centerish - was +80000,-55000
 
       //domainOutpost.setOptionalUniqueId("oldlegion_domint_outpost");
-        domainOutpost.addTag(Tags.THEME_HIDDEN);
-        domainOutpost.addTag(Tags.THEME_SPECIAL);
-        domainOutpost.addTag(Tags.THEME_UNSAFE);
-        domainOutpost.addTag(Tags.NOT_RANDOM_MISSION_TARGET);
+        domainOutpostSystem.addTag(Tags.THEME_HIDDEN);
+        domainOutpostSystem.addTag(Tags.THEME_SPECIAL);
+        domainOutpostSystem.addTag(Tags.THEME_UNSAFE);
+        domainOutpostSystem.addTag(Tags.NOT_RANDOM_MISSION_TARGET);
 
-        domainOutpost.setBackgroundTextureFilename("graphics/mymod/backgrounds/mybackground.jpg");
+        domainOutpostSystem.setBackgroundTextureFilename("graphics/mymod/backgrounds/mybackground.jpg");
 
-        PlanetAPI argonStar = domainOutpost.initStar("Argonian", // unique id for this star
-                "star_red_giant", // id in planets.json
-                100f, // radius (in pixels at default zoom)
-                650); // corona radius, from star edge
-        domainOutpost.setLightColor(new Color(239, 155, 128)); // light color in entire DomainOutpost, affects all entities
+        domainOutpostSystem.initNonStarCenter();
+//        PlanetAPI argonStar = domainOutpost.initStar("Argonian", // unique id for this star
+//                "star_red_giant", // id in planets.json
+//                100f, // radius (in pixels at default zoom)
+//                650); // corona radius, from star edge
+//        domainOutpost.setLightColor(new Color(239, 155, 128)); // light color in entire DomainOutpost, affects all entities
+//        domainOutpost.removeEntity(argonStar);
 
-        domainOutpost.removeEntity(argonStar);
-        SectorEntityToken relay = domainOutpost.addCustomEntity("mam_relay", "Domain Comm Relay", "comm_relay", "domainspecops");
+        SectorEntityToken relay = domainOutpostSystem.addCustomEntity("mam_relay", "Domain Comm Relay", "comm_relay_domint", "domainspecops");
+
+        SectorEntityToken relay_field = domainOutpostSystem.addTerrain(Terrain.MAGNETIC_FIELD,
+                new MagneticFieldTerrainPlugin.MagneticFieldParams(
+                        relay.getRadius() + 150f, // terrain effect band width
+                        (relay.getRadius() + 150f), // terrain effect middle radius
+                        relay, // entity that it's around
+                        relay.getRadius()-25f, // visual band start
+                        relay.getRadius() + 50f + 100f, // visual band end
+                        new Color(149, 149, 149, 50), // base color
+                        0.75f, // probability to spawn aurora sequence, checked once/day when no aurora in progress
+                        new Color(90, 180, 40),
+                        new Color(130, 145, 90),
+                        new Color(165, 110, 145),
+                        new Color(95, 55, 160),
+                        new Color(117, 104, 140), // 45, 0, 130
+                        new Color(128, 128, 128), // 20, 0, 130
+                        new Color(154, 154, 154))); // 10, 0, 150
+        relay_field.setCircularOrbit(relay, 0, 0, 100);
+
+        SectorEntityToken relay_field1 = domainOutpostSystem.addTerrain(Terrain.MAGNETIC_FIELD,
+                new MagneticFieldTerrainPlugin.MagneticFieldParams(
+                        relay.getRadius() + 150f, // terrain effect band width
+                        (relay.getRadius() + 150f), // terrain effect middle radius
+                        relay, // entity that it's around
+                        relay.getRadius()+25f, // visual band start
+                        relay.getRadius() + 50f + 200f, // visual band end
+                        new Color(149, 149, 149, 50), // base color
+                        0.75f, // probability to spawn aurora sequence, checked once/day when no aurora in progress
+                        new Color(90, 180, 40),
+                        new Color(130, 145, 90),
+                        new Color(165, 110, 145),
+                        new Color(95, 55, 160),
+                        new Color(117, 104, 140), // 45, 0, 130
+                        new Color(128, 128, 128), // 20, 0, 130
+                        new Color(154, 154, 154))); // 10, 0, 150
+        relay_field1.setCircularOrbit(relay, 0, 0, 100);
+
+        SectorEntityToken relay_field2 = domainOutpostSystem.addTerrain(Terrain.MAGNETIC_FIELD,
+                new MagneticFieldTerrainPlugin.MagneticFieldParams(relay.getRadius() + 150f, // terrain effect band width
+                        (relay.getRadius() + 150f), // terrain effect middle radius
+                        relay, // entity that it's around
+                        relay.getRadius() + 50f, // visual band start
+                        relay.getRadius() + 50f + 500f, // visual band end
+                        new Color(149, 149, 149, 50), // base color
+                        0.75f, // probability to spawn aurora sequence, checked once/day when no aurora in progress
+                        new Color(90, 180, 40),
+                        new Color(130, 145, 90),
+                        new Color(165, 110, 145),
+                        new Color(95, 55, 160),
+                        new Color(117, 104, 140), // 45, 0, 130
+                        new Color(128, 128, 128), // 20, 0, 130
+                        new Color(154, 154, 154))); // 10, 0, 150
+        relay_field2.setCircularOrbit(relay, 0, 0, 100);
+
+        SectorEntityToken relay_field3 = domainOutpostSystem.addTerrain(Terrain.MAGNETIC_FIELD,
+                new MagneticFieldTerrainPlugin.MagneticFieldParams(relay.getRadius() + 300f, // terrain effect band width
+                        (relay.getRadius() + 200f), // terrain effect middle radius
+                        relay, // entity that it's around
+                        relay.getRadius() + 100f, // visual band start
+                        relay.getRadius() + 50f + 500f, // visual band end
+                        new Color(149, 149, 149, 50), // base color
+                        0.75f, // probability to spawn aurora sequence, checked once/day when no aurora in progress
+                        new Color(90, 180, 40),
+                        new Color(130, 145, 90),
+                        new Color(165, 110, 145),
+                        new Color(95, 55, 160),
+                        new Color(117, 104, 140), // 45, 0, 130
+                        new Color(128, 128, 128), // 20, 0, 130
+                        new Color(154, 154, 154))); // 10, 0, 150
+        relay_field3.setCircularOrbit(relay, 0, 0, 100);
 
         //asteroid belt1 ring
         //domainOutpost.addAsteroidBelt(relay, 1000, asteroidBelt1Dist, 800, 250, 400, Terrain.ASTEROID_BELT, "Inner Band");
@@ -56,7 +129,7 @@ public class MySystemOne {
         //domainOutpost.addRingBand(relay, "misc", "rings_asteroids0", 256f, 0, Color.gray, 256f, asteroidBelt1Dist, 350f);
         //domainOutpost.addRingBand(relay, "misc", "rings_asteroids0", 256f, 2, Color.gray, 256f, asteroidBelt1Dist + 200, 400f);
 
-        PlanetAPI Hunhow = domainOutpost.addPlanet("Hunhow", relay, "Hunhow's Fall", "barren", 0, 273, 4977, 157);
+        PlanetAPI Hunhow = domainOutpostSystem.addPlanet("Hunhow", relay, "Hunhow's Fall", "barren", 0, 273, 4977, 157);
         Hunhow.setFaction("domainspecops");
         MarketAPI market = Global.getFactory().createMarket(
                 "Hunhow_market",
@@ -117,23 +190,23 @@ public class MySystemOne {
         market.addTag("market_no_officer_spawn");
         Global.getSector().getEconomy().addMarket(market, false); //the ''WithJunkerAndChatter'' flag. it will add space debris in orbit and radio chatter sound effects.
 
-        SectorEntityToken buoy = domainOutpost.addCustomEntity("nav_buoy", "Nav Buoy", "nav_buoy", "domainspecops");
+        SectorEntityToken buoy = domainOutpostSystem.addCustomEntity("nav_buoy", "Nav Buoy", "nav_buoy", "domainspecops");
         buoy.setCircularOrbit(relay, 25, 6561, 315);
 
-        SectorEntityToken array = domainOutpost.addCustomEntity("sensor_array", "Sensor Array", "sensor_array", "domainspecops");
+        SectorEntityToken array = domainOutpostSystem.addCustomEntity("sensor_array", "Sensor Array", "sensor_array", "domainspecops");
         array.setCircularOrbit(relay, 25, 2361, 93);
 
-        SectorEntityToken stableLoc1 = domainOutpost.addCustomEntity("argon_stableloc_1", "Stable Location", "stable_location", Factions.NEUTRAL);
+        SectorEntityToken stableLoc1 = domainOutpostSystem.addCustomEntity("argon_stableloc_1", "Stable Location", "stable_location", Factions.NEUTRAL);
         stableLoc1.setCircularOrbit(relay, MathUtils.getRandomNumberInRange(0f, 360f), 3700f, 520);
 
-        SectorEntityToken domaingate = domainOutpost.addCustomEntity("domain_ops_gate", "Domain Gate", "inactive_gate", "domainspecops");
+        SectorEntityToken domaingate = domainOutpostSystem.addCustomEntity("domain_ops_gate", "Domain Gate", "inactive_gate", "domainspecops");
         domaingate.setCircularOrbit(relay, 10, 5736, 273);
 
-        SectorEntityToken beacon = domainOutpost.addCustomEntity("warning_beacon_grave","Warning Beacon", "warning_beacon","domainspecops");
+        SectorEntityToken beacon = domainOutpostSystem.addCustomEntity("warning_beacon_grave","Warning Beacon", "warning_beacon","domainspecops");
         beacon.setCircularOrbit(relay,2,7934,1256);
         beacon.setCustomDescriptionId("graveyard_beacon");
 
-        SectorEntityToken argonAF1 = domainOutpost.addTerrain(Terrain.ASTEROID_FIELD,
+        SectorEntityToken argonAF1 = domainOutpostSystem.addTerrain(Terrain.ASTEROID_FIELD,
                 new AsteroidFieldTerrainPlugin.AsteroidFieldParams(
                         300f, // min radius
                         500f, // max radius
@@ -152,35 +225,35 @@ public class MySystemOne {
         params.source = DebrisFieldTerrainPlugin.DebrisFieldSource.MIXED;
         params.baseSalvageXP = 250; // base XP for scavenging in field
 
-        SectorEntityToken debrisNextToGate = Misc.addDebrisField(domainOutpost, params, StarSystemGenerator.random);
+        SectorEntityToken debrisNextToGate = Misc.addDebrisField(domainOutpostSystem, params, StarSystemGenerator.random);
         debrisNextToGate.setSensorProfile(1000f);
         debrisNextToGate.setDiscoverable(true);
         debrisNextToGate.setCircularOrbit(beacon, 0f, 0f, 130f);
         debrisNextToGate.setId("domain_debrisNextToBeacon");
 
         SectorEntityToken Arcon_A = OldLegionMisc.addDerelict(
-                domainOutpost, //Star system
-                domainOutpost.getEntityById("warning_beacon_grave"), //Orbital focus
+                domainOutpostSystem, //Star system
+                domainOutpostSystem.getEntityById("warning_beacon_grave"), //Orbital focus
                 "onslaught_xiv_Elite", //Variant Id
                 ShipRecoverySpecial.ShipCondition.BATTERED, //Ship condition
                 100f, //Orbital radius
                 true //Recoverable?
         );
-        SectorEntityToken Arcon_1 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 234f, true);
-        SectorEntityToken Arcon_2 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 407f, true);
-        SectorEntityToken Arcon_3 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 458f, true);
-        SectorEntityToken Arcon_5 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "legion_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 363f, true);
-        SectorEntityToken Arcon_6 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "legion_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 171f, true);
-        SectorEntityToken Arcon_7 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 281f, true);
-        SectorEntityToken Arcon_8 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 384f, true);
-        SectorEntityToken Arcon_9 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 432f, true);
-        SectorEntityToken Arcon_10 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 331f, true);
-        SectorEntityToken Arcon_11 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 397f, true);
-        SectorEntityToken Arcon_12 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 303f, true);
-        SectorEntityToken Arcon_13 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 91f, true);
-        SectorEntityToken Arcon_14 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 121f, true);
-        SectorEntityToken Arcon_15 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 481f, true);
-        SectorEntityToken Arcon_16 = OldLegionMisc.addDerelict(domainOutpost, domainOutpost.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 394f, true);
+        SectorEntityToken Arcon_1 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 234f, true);
+        SectorEntityToken Arcon_2 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 407f, true);
+        SectorEntityToken Arcon_3 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "onslaught_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 458f, true);
+        SectorEntityToken Arcon_5 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "legion_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 363f, true);
+        SectorEntityToken Arcon_6 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "legion_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 171f, true);
+        SectorEntityToken Arcon_7 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 281f, true);
+        SectorEntityToken Arcon_8 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 384f, true);
+        SectorEntityToken Arcon_9 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "dominator_XIV_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 432f, true);
+        SectorEntityToken Arcon_10 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 331f, true);
+        SectorEntityToken Arcon_11 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 397f, true);
+        SectorEntityToken Arcon_12 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "falcon_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 303f, true);
+        SectorEntityToken Arcon_13 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 91f, true);
+        SectorEntityToken Arcon_14 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 121f, true);
+        SectorEntityToken Arcon_15 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 481f, true);
+        SectorEntityToken Arcon_16 = OldLegionMisc.addDerelict(domainOutpostSystem, domainOutpostSystem.getEntityById("warning_beacon_grave"), "eagle_xiv_Elite", ShipRecoverySpecial.ShipCondition.WRECKED, 394f, true);
 
         Global.getSector().addScript(new ArtanisPersonalFleet());
     }
