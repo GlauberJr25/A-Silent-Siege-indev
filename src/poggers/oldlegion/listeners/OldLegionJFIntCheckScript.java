a65659bed3085c759089a81b1859421d5d45e9cd
NebulaModLab <NebulaModding@hotmail.com>
2025-08-25 03:57:03 +0200

    Switched dialog to use rules.csv ecosystem

--- src/poggers/oldlegion/listeners/OldLegionJFIntCheckScript.java
+++ src/poggers/oldlegion/listeners/OldLegionJFIntCheckScript.java
@@ -6,6 +6,7 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
+import com.fs.starfarer.api.util.Misc;
 import poggers.oldlegion.campaign.plugins.NatarukIntroTestPlugin;
 
 public class OldLegionJFIntCheckScript implements EveryFrameScript {
@@ -18,7 +19,10 @@ public class OldLegionJFIntCheckScript implements EveryFrameScript {
             } else if (currSystem != null && currSystem.getName().contains("Nataruk")) {
                 if (!Global.getSector().getMemoryWithoutUpdate().contains("$oldlegion_IntroExpositionDone")) {
                     Global.getSector().getMemoryWithoutUpdate().set("$oldlegion_IntroExpositionDone", true);
-                    Global.getSector().getCampaignUI().showInteractionDialog(new NatarukIntroTestPlugin(), (SectorEntityToken)null);
+//                    Global.getSector().getCampaignUI().showInteractionDialog(new NatarukIntroTestPlugin(), (SectorEntityToken)null);
+                    // Rules.csv way of triggering dialog allowing full use of its system
+                    CampaignFleetAPI pf = Global.getSector().getPlayerFleet();
+                    Misc.showRuleDialog(pf, "YourInitialTrigger"); // YourInitialTrigger = initial trigger
                 }
             }
         }
